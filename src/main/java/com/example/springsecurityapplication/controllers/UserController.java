b9b669fdf77babb862e773179db44e34a30b7947
NordTort <41715818+NorthTort@users.noreply.github.com>
2022-12-08 02:08:28 +0300

    Добавили в index.html отображение товаров в личном кабинете авторизованным польхователям с ролью ROLE_USER Прописали логику в UserController

--- src/main/java/com/example/springsecurityapplication/controllers/UserController.java
+++ src/main/java/com/example/springsecurityapplication/controllers/UserController.java
@@ -1,15 +1,26 @@
 package com.example.springsecurityapplication.controllers;
 
 import com.example.springsecurityapplication.security.PersonDetails;
+import com.example.springsecurityapplication.servises.ProductServise;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UserController {
+
+    private final ProductServise productServise;
+
+    public UserController(ProductServise productServise) {
+        this.productServise = productServise;
+    }
+
+
     @GetMapping("/index")
-    public String index(){
+    public String index(Model model){
 //        Получем объект аутентификации, и с помощью SecurityContextHolder.getContext() обращаемся к контексту и вызываем на нем метод аутентификации
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
@@ -22,6 +33,14 @@ public class UserController {
             return "redirect:/admin";
         }
 
+        model.addAttribute("products", productServise.getAllProduct());
+
         return "user/index";
+    }
+
+    @GetMapping("/info/{id}")
+    public String infoProduct(@PathVariable("id") int id, Model model){
+        model.addAttribute("product", productServise.getProductId(id));
+        return "product/infoProduct";
     }
 }
